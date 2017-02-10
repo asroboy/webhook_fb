42c000e47e3440158b65f2154368e908836dfdc8
Asrofi Ridho <Asrofi Ridho>
2017-02-10 20:15:14 +0700

    halo

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/webhook', function (req, res) {
 				if(event.message.quick_reply.payload === 'REGISTER_PAYLOAD'){
 					getResponseToUser(event.message.quick_reply.payload, event.sender.id, event.recipient.id);
 				}else if(payload_prefix==='DEVELOPER'){
-					getResponseToUser(event.message.quick_reply.text, event.sender.id, event.recipient.id);
+					getResponseToUser(event.message.text, event.sender.id, event.recipient.id);
 				}else if(event.message.quick_reply.payload){
 					//var token = "";
 					getToken(event.message.quick_reply.payload,event.recipient.id, event.sender.id)
