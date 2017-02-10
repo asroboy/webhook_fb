7593a87179e6dd4b59f82c8b10c07535ea92e350
Asrofi Ridho <Asrofi Ridho>
2017-02-10 19:55:54 +0700

    halo

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook', function (req, res) {
 					getResponseToUser(event.message.quick_reply.payload, event.sender.id, event.recipient.id);
 				}else if(event.message.quick_reply.payload){
 					//var token = "";
-					getToken(event.message.quick_reply.payload,event.sender.id, event.recipient.id)
+					getToken(event.message.quick_reply.payload,event.recipient.id, event.sender.id)
 					//firstMessage(event.sender.id);
 				}
 			}else{
